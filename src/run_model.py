ec9666dae523552e4f190ae189354df34b4aa337
kishore-talentsprint <kishore.n@talentsprint.com>
2024-10-14 18:45:38 +0530

    Update run_model.py

--- src/run_model.py
+++ src/run_model.py
@@ -16,17 +16,8 @@ def load_model():
 
 def get_customer_input():
     credit_score = float(input("Enter credit score: "))
-    age = int(input("Enter age: "))
-    balance = float(input("Enter balance: "))
-    number_of_products = int(input("Enter number of products: "))
-    geography = int(input('Enter the location (as number):'))
-    gender = int(input('Enter the gender of the customer (0 - Male, 1 - Female):'))
-    tenure = int(input('Tenure in years:'))
-    has_card = int(input('Do you have a credit card? (1 - yes, 0 - no)'))
-    # is_active_member = int(input('member of back (1 - yes, 0 - no):'))
-    
     # Add other fields as necessary
-    return [[credit_score, geography, gender, age, tenure, balance, number_of_products, has_card]]
+    return [[credit_score]]
 
 def predict_churn(model, customer_data):
     prediction = model.predict(customer_data)
