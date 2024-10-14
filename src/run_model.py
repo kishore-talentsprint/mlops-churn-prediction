f16771236fcaabca5699b8ba326f636c840a8949
kishore-talentsprint <kishore.n@talentsprint.com>
2024-10-14 08:57:02 +0000

    set up mk1

--- src/run_model.py
+++ src/run_model.py
@@ -2,7 +2,7 @@ import os
 import pickle
 
 def load_model():
-    model_path = os.path.join('models', 'churn_model.pkl')
+    model_path = '/workspaces/mlops-churn-prediction/models/churn_model.pkl' #os.path.join('models', 'churn_model.pkl')
     with open(model_path, 'rb') as model_file:
         model = pickle.load(model_file)
     return model
