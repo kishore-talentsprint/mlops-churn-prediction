f868f5e50d315b646a951545b587a30ab5399df5
kishore-talentsprint <kishore.n@talentsprint.com>
2024-10-14 17:39:46 +0530

    Updated load_model.py

--- src/load_model.py
+++ src/load_model.py
@@ -1,5 +1,12 @@
 import pickle
 import os
+import sys
+
+
+from pathlib import Path
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
 
 def load_model():
     model_path = os.path.join('models', 'churn_model.pkl')
